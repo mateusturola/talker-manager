6fc963be913a57df05184831d1b7f435b263091d
mateusturola <turolamateus@gmail.com>
2022-03-18 20:32:58 -0300

    style: ajusta lint

--- Middlewares/login.js
+++ Middlewares/login.js
@@ -5,18 +5,19 @@ const validateEmailFormat = (email) => {
 
 const validateInput = (req, res, next) => {
   const { email, password } = req.body;
-  if (!email) {
-    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
-  }
+  
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  
   if (!validateEmailFormat(email)) {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
-  if (!password) {
-    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
-  }
+  
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+
   if (password.length < 6) {
     return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
+  
   next();
 };
 
