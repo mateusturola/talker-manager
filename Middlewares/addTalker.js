05ca59d5b5509a5c83cafddd63d0d7caf500e154
mateusturola <turolamateus@gmail.com>
2022-03-19 15:04:53 -0300

    refactor: Modifica modo de inserção de novos palestrantes

--- Middlewares/addTalker.js
+++ Middlewares/addTalker.js
@@ -4,17 +4,15 @@ const writeFile = require('../helpers/writeFile');
 const addTalker = async (req, res) => {
   const { body } = req;
   const talkers = await readFile('./talker.json');
-  // console.log('talkers', talkers);
   
   const newUser = {
     ...body,
     id: talkers.length + 1,
   };
-  talkers.push(newUser);
 
-  // const users = [...talkers, newUser]; 
+  const users = [...talkers, newUser]; 
 
-  await writeFile('./talker.json', talkers);
+  await writeFile('./talker.json', users);
 
   res.status(201).json(newUser);
 };
